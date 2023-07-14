01beb10762ea9042a3e8fd9680043920912c9352
karimzakir02 <zakirkarim02@gmail.com>
2023-07-14 08:00:01 -0400

    Change example to gs.com

--- example_3.py
+++ example_3.py
@@ -2,17 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-link = "https://www.recursion.com/careers?gh_jid=4343702"
+link = "https://goldmansachs.tal.net/vx/lang-en-GB/mobile-0/brand-2/xf-caeaaeb33c61/candidate/register"
 
 driver = webdriver.Chrome()
 driver.set_window_size(1920, 1080)
 driver.get(link)
 time.sleep(1)
 
-driver.switch_to.frame(driver.find_element(By.ID, "grnhse_iframe"))
+first_name_box = driver.find_element(By.XPATH, '//*[@id="register_form_firstname"]')
+first_name_box.send_keys("Karim")
 
-why_box = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/form/div[2]/div[7]/label/textarea')
-why_box.send_keys("I hear the tech talks are pretty good :)")
+last_name_box = driver.find_element(By.XPATH, '//*[@id="register_form_lastname"]')
+last_name_box.send_keys("Zakir")
+
+email_box = driver.find_element(By.XPATH, '//*[@id="register_form_new_user"]')
+email_box.send_keys("zakirkarim02@gmail.com")
 
 time.sleep(100)
 
