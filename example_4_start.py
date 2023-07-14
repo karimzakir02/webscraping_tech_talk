10dae7b95aa69385b68f3f783453f8c140e5f10d
karimzakir02 <zakirkarim02@gmail.com>
2023-07-14 08:00:49 -0400

    Add hint

--- example_4_start.py
+++ example_4_start.py
@@ -13,7 +13,7 @@ json_payload = {
     "pagination": {
         "from": 0,
         "size": 48
-    },
+    }, 
     "banner": "loblaw",
     "cartId": "c4f214ee-a938-4f35-873b-1e9073804b3e",
     "lang": "en",
@@ -32,3 +32,4 @@ response = requests.post(link,
                          json=json_payload,
                          verify=False)
 print(response.status_code)
+# .json()["results"]
